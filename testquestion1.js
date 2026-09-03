26b85ee88c7f9210e8c7e06172c6e031b013ac35
Sebastian1234567891011121314151 <sebyplaysroblox@gmail.com>
2026-09-03 11:49:54 +0000

    Sep 3, 2026, 11:49 AM

--- testquestion1.js
+++ testquestion1.js
@@ -6,7 +6,7 @@ function setup(){
     new Canvas(600,400);
     background("blue");
     world.gravity.y=10;
-    player=new Sprite(0,0,30,30);
+    player=new Sprite(300,200,30,30);
     
     floor =new Sprite(500,350,2400,20,"static");
 
